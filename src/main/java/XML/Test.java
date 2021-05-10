54729c04e49ac1dce297ad0ff4d4d10e3728e4c3
RazorSun <oreoalex24@gmail.com>
2021-05-10 21:07:33 +0800

    XML TO POJO

--- src/main/java/XML/Test.java
+++ src/main/java/XML/Test.java
@@ -3,6 +3,8 @@ package XML;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
@@ -11,7 +13,12 @@ public class Test {
         JAXBContext context = JAXBContext.newInstance(GXML.class);
         Marshaller marshaller = context.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-        GXML gxml = new GXML("1","2","3");
-        marshaller.marshal(gxml,new FileOutputStream("example.xml"));
+        GXML gxml = new GXML("12", "21", "32");
+        marshaller.marshal(gxml, new FileOutputStream("example.xml"));
+        Unmarshaller unmarshal = context.createUnmarshaller();
+        File file = new File("example.xml");
+        GXML gxml2 = (GXML) unmarshal.unmarshal(file);
+        System.out.println(gxml2.toString());
+
     }
 }
