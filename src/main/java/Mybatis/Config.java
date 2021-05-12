2bed6d80ee3b45bb5ae49c945440d10308188325
RazorSun <oreoalex24@gmail.com>
2021-05-12 21:16:13 +0800

    mybatis  - 1

---
+++ src/main/java/Mybatis/Config.java
@@ -0,0 +1,5 @@
+package Mybatis;
+
+public class Config {
+
+}
---
+++ src/main/java/Mybatis/Demo1.java
@@ -0,0 +1,16 @@
+package Mybatis;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Demo1 {
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    }
+}
