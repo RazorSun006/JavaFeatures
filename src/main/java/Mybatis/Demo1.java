16f03fbeb7c6120f7b4b729fea797fec44a9403a
RazorSnow <changjiangmingxj@163.com>
2021-05-12 22:00:38 +0800

    the first mybatis demo

--- src/main/java/Mybatis/Demo1.java
+++ src/main/java/Mybatis/Demo1.java
@@ -1,6 +1,7 @@
 package Mybatis;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
@@ -12,5 +13,8 @@ public class Demo1 {
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        Employee employee = sqlSession.selectOne("Mybatis.Employee.selectEmp", 1);
+
     }
 }
